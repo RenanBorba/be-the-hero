2ff8b75ea5dedef0fb65c38371863bd6a5ac0e50
Renan Borba <renandbm.rb@gmail.com>
2020-03-29 20:44:53 -0300

    Mobile (src/routes, pages/Incidents, pages/Detail)

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,79 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Fragment } from 'react';
+import
+  { View,
+    StatusBar,
+    Text,
+    Image,
+    TouchableOpacity
+  } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
 
 export default function Detail() {
   return (
-    <View />
+    <Fragment>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#F2F2F2"
+      />
+
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={logoImg} />
+          <TouchableOpacity onPress={() => {}}>
+            <Feather
+              name="arrow-left"
+              size={28}
+              color="#E02041"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.incident}>
+          <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+          <Text style={styles.incidentValue}>APAD</Text>
+
+          <Text style={styles.incidentProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+          <Text style={styles.incidentProperty}>Valor:</Text>
+          <Text style={styles.incidentValue}>R$ 120,00</Text>
+        </View>
+
+        {/* IncidentContactBox */}
+        <View style={[styles.incident, {marginTop: 0}]}>
+          <Text style={styles.heroTitle}>Salve o dia!</Text>
+          <Text style={styles.heroTitle}>
+            Seja o herói desse caso.
+          </Text>
+
+          <Text style={styles.heroContact}>
+            Entre em contato:
+          </Text>
+
+          <View style={styles.actionsContact}>
+            <TouchableOpacity
+              onPress= {() => {}}
+              style={styles.action}
+            >
+              <Text style={styles.actionText}>
+                Whatsapp
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress= {() => {}}
+              style={styles.action}
+            >
+              <Text style={styles.actionText}>
+                E-mail
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Fragment>
   );
 };
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,82 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Fragment } from 'react';
+import
+  {
+    View,
+    StatusBar,
+    FlatList,
+    Text,
+    Image,
+    TouchableOpacity
+  } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    // Navegar para Detail
+    navigation.navigate('Detail');
+  }
+
   return (
-    <View />
+    <Fragment>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#F2F2F2"
+      />
+
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={logoImg} />
+          <Text style={styles.headerText}>
+            Total de <Text style={styles.headerTextBold} />0 casos.
+          </Text>
+        </View>
+
+        <Text style={styles.title}>Bem vindo!</Text>
+        <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+        <FlatList
+          data={[ 1, 2, 3, 4 ]}
+          style={styles.incidentList}
+          keyExtractor=
+            { incident => String( incident )}
+          showsVerticalScrollIndicator={false}
+          renderItem={() => (
+            /* Incident */
+            <View style={styles.incident}>
+              <Text style={styles.incidentProperty}>ONG:</Text>
+              <Text style={styles.incidentValue}>APAD</Text>
+
+              <Text style={styles.incidentProperty}>CASO:</Text>
+              <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+              <Text style={styles.incidentProperty}>VALOR:</Text>
+              <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+              <TouchableOpacity
+                onPress={ navigateToDetail }
+                style={styles.detailsButton}
+              >
+                <Text style={styles.detailsButtonText}>
+                  VER MAIS DETALHES
+                </Text>
+
+                <Feather
+                  name="arrow-right"
+                  size={17}
+                  color="#E02041"
+                  style={styles.arrowRight}
+                />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
+    </Fragment>
   );
 };
